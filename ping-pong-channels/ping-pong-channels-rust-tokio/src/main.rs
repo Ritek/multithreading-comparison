9c2d318aa3f9be4b11f7a1f7c3fa9d987e9e6018
Ritek <writhaler@gmail.com>
2026-05-05 23:37:23 +0100

    Add example test to rust-tokio-channel-ping-pong

--- ping-pong-channels/ping-pong-channels-rust-tokio/src/main.rs
+++ ping-pong-channels/ping-pong-channels-rust-tokio/src/main.rs
@@ -11,22 +11,16 @@ struct GameStateMessage {
     counter: usize,
 }
 
-fn try_hit() -> bool {
-    let nanos = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .unwrap()
-        .subsec_nanos();
-
-    nanos % 100 != 99
-}
-
 async fn player(
     name: String,
     mut receiver: tokio::sync::mpsc::Receiver<GameStateMessage>,
     sender: tokio::sync::mpsc::Sender<GameStateMessage>,
+    engine: std::sync::Arc<dyn GameEngine>,
     is_starter: bool,
 ) {
     if is_starter {
+        engine.log_event(&format!("{} hit {:?} (count: {})", name, Ball::Ping, 1));
+
         let _ = sender
             .send(GameStateMessage {
                 ball: Ball::Ping,
@@ -41,7 +35,7 @@ async fn player(
             break;
         }
 
-        if !try_hit() {
+        if !engine.should_hit(&name) {
             println!("{} dropped the ball!", name);
             let _ = sender
                 .send(GameStateMessage {
@@ -58,7 +52,10 @@ async fn player(
             _ => Ball::Ping,
         };
 
-        println!("{} hit {:?} (count: {})", name, msg.ball, msg.counter);
+        engine.log_event(&format!(
+            "{} hit {:?} (count: {})",
+            name, msg.ball, msg.counter
+        ));
 
         if sender.send(msg).await.is_err() {
             break;
@@ -66,13 +63,86 @@ async fn player(
     }
 }
 
-#[tokio::main]
-async fn main() -> () {
+trait GameEngine: Send + Sync {
+    fn should_hit(&self, player_name: &str) -> bool;
+    fn log_event(&self, message: &str);
+}
+
+struct RealEngine;
+impl GameEngine for RealEngine {
+    fn should_hit(&self, _: &str) -> bool {
+        std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .subsec_nanos()
+            % 10
+            != 0
+    }
+    fn log_event(&self, m: &str) {
+        println!("[PROD] {}", m);
+    }
+}
+
+struct MockEngine {
+    max_hits: usize,
+    hits_count: std::sync::atomic::AtomicUsize,
+}
+impl GameEngine for MockEngine {
+    fn should_hit(&self, _: &str) -> bool {
+        let current = self
+            .hits_count
+            .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        current < self.max_hits
+    }
+    fn log_event(&self, m: &str) {
+        eprintln!("[TEST] {}", m);
+    }
+}
+
+async fn run_game(dependencies: std::sync::Arc<dyn GameEngine>) {
     let (p1_send, p1_receive) = tokio::sync::mpsc::channel::<GameStateMessage>(1);
     let (p2_send, p2_receive) = tokio::sync::mpsc::channel::<GameStateMessage>(1);
 
-    let player1 = tokio::spawn(player(String::from("Player1"), p1_receive, p2_send, true));
-    let player2 = tokio::spawn(player(String::from("Player2"), p2_receive, p1_send, false));
+    let player1 = tokio::spawn(player(
+        String::from("Player1"),
+        p1_receive,
+        p2_send,
+        dependencies.clone(),
+        true,
+    ));
+    let player2 = tokio::spawn(player(
+        String::from("Player2"),
+        p2_receive,
+        p1_send,
+        dependencies.clone(),
+        false,
+    ));
 
     let _ = tokio::join!(player1, player2);
+}
+
+#[tokio::main]
+async fn main() -> () {
+    let dependencies = std::sync::Arc::new(RealEngine);
+    run_game(dependencies).await;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_fixed_length_game() {
+        let engine = std::sync::Arc::new(MockEngine {
+            max_hits: 4,
+            hits_count: std::sync::atomic::AtomicUsize::new(0),
+        });
+
+        run_game(engine.clone()).await;
+
+        assert_eq!(
+            engine.hits_count.load(std::sync::atomic::Ordering::SeqCst),
+            5
+        );
+    }
 }
